919587d0e2afa1e8f7df0b2d1bcaf521ae76f34c
Doug Schmidt <doug.schmidt@aquaticinformatics.com>
2018-03-01 15:57:14 -0800

    Issue-23 Added RBridgeTool and ReportTool as artifacts. Fixed the assembly versions

--- src/Reports.ReportPluginBase/Properties/AssemblyInfo.cs
+++ src/Reports.ReportPluginBase/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("My Report")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018 Aquatic Informatics Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
