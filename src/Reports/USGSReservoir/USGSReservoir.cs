e1e13dc5af4bdf3db247b4c74eddf0de45435bc3
Kim Sharpe <kim.sharpe@aquaticinformatics.com>
2018-10-30 09:54:51 -0700

    Kim022-Add-3-Sierra-Hydrographics-Custom-Reports-For-18.3  First check-in of 3 new custom reports for Sierra Hydrographics  1. Daily Mean Discharge  2. Daily Midnight Contents  3. USGS Reservoir

---
+++ src/Reports/USGSReservoir/USGSReservoir.cs
@@ -0,0 +1,12 @@
+﻿using ReportPluginFramework.Beta;
+
+namespace Reports
+{
+    public class USGSReservoir : USGSReservoirNamespace.ReportPluginBase, IReport
+    {
+        public override void AddReportSpecificTables(System.Data.DataSet dataSet)
+        {
+            USGSReservoirNamespace.ReportSpecificTablesBuilder.AddReportSpecificTables(dataSet);
+        }
+    }
+}
