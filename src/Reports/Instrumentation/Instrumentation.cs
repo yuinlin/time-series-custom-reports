c6826cf5a181501faac5e018400fc019b9f53ece
Kim Sharpe <kim.sharpe@aquaticinformatics.com>
2019-01-20 16:33:06 -0800

    Kim028-Add-2-WSC-Reports-Instrumentation-And-R56-Discharge-Measurements add 2 new custom WSC reports: Instrumentation and Discharge Measurements R56 update Field Activity report so it is consisent with the 2 new reports address feedback comments from WSC: Everywhere shown use superscript 3 (cubed) for Discharge units Field Activity column value for "DischargeSummary" should have blank separator Do not check publish flag for WaterTemp and AirTemp in DischargeMeasurementsR56

---
+++ src/Reports/Instrumentation/Instrumentation.cs
@@ -0,0 +1,12 @@
+﻿using ReportPluginFramework.Beta;
+
+namespace Reports
+{
+    public class Instrumentation : InstrumentationNamespace.ReportPluginBase, IReport
+    {
+        public override void AddReportSpecificTables(System.Data.DataSet dataSet)
+        {
+            InstrumentationNamespace.ReportSpecificTablesBuilder.AddReportSpecificTables(dataSet);
+        }
+    }
+}
