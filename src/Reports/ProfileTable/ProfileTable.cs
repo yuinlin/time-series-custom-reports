05e824631b6c42ad1f1ab0d7e66b8aca99ad90a3
Kim Sharpe <kim.sharpe@aquaticinformatics.com>
2019-08-19 10:25:25 -0700

    Kim041-Add-New-Report-ThamesWater-ProfileTable First check-in of profile report for Thames Water, an upgrade 3.x to NG Still needs thorough testing on NG because it relies on Gaps and these changed

---
+++ src/Reports/ProfileTable/ProfileTable.cs
@@ -0,0 +1,12 @@
+﻿using ReportPluginFramework.Beta;
+
+namespace Reports
+{
+    public class ProfileTable : ProfileTableNamespace.ReportPluginBase, IFileReport
+    {
+        public override void AddReportSpecificTables(System.Data.DataSet dataSet)
+        {
+            ProfileTableNamespace.ReportSpecificTablesBuilder.AddReportSpecificTables(dataSet);
+        }
+    }
+}
---
+++ src/Reports/ProfileTable/ReportSpecificTableBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using System.Reflection;
+using ReportPluginFramework.Beta;
+using ReportPluginFramework.Beta.ReportData;
+using ReportPluginFramework.Beta.ReportData.TimeSeriesComputedStatistics;
+using ReportPluginFramework.Beta.ReportData.TimeSeriesData;
+using ReportPluginFramework.Beta.ReportData.TimeSeriesDescription;
+using System.Collections.Generic;
+
+namespace ProfileTableNamespace
+{
+    public class ReportSpecificTablesBuilder
+    {
+        private static ServiceStack.Logging.ILog Log = ServiceStack.Logging.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static void AddReportSpecificTables(DataSet dataSet)
+        {
+            try
+            {
+                Common common = (Common)dataSet.Tables["RunReportRequest"].Rows[0]["CommonLibrary"];
+
+                DataTable settingsTable = dataSet.Tables["ReportSettings"];
+                settingsTable.Columns.Add("ReportTitle", typeof(string));
+                settingsTable.Rows[0]["ReportTitle"] = "Profile Table";
+
+                DataTable table = new DataTable("ProfileTableDataTable");
+
+                dataSet.Tables.Add(table);
+            }
+            catch (Exception exp)
+            {
+                Log.Error("Error creating report specific data tables ", exp);
+                throw exp;
+            }
+        }
+    }
+}
