e8700aa20055385c84f6995c1dec1bfad6210f6b
Kim Sharpe <kim.sharpe@aquaticinformatics.com>
2019-11-08 16:02:43 -0800

    Kim044-Update-Dlls-19.3-And-Add-New-Report-NeDNR-USGS-Daily-Mean 1. Update Aqts dlls to 19.3.80 2. First check-in of new report for NeDNR - USGS Daily Mean 3. Modify report projects to reduce unneeded copies of large PerpetuumSoft dll

---
+++ src/Reports/USGSDailyMean/ReportSpecificTableBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using System.Reflection;
+using ReportPluginFramework.Beta;
+using ReportPluginFramework.Beta.ReportData;
+using ReportPluginFramework.Beta.ReportData.TimeSeriesComputedStatistics;
+using ReportPluginFramework.Beta.ReportData.TimeSeriesData;
+using ReportPluginFramework.Beta.ReportData.TimeSeriesDescription;
+using System.Collections.Generic;
+
+namespace USGSDailyMeanNamespace
+{
+    public class ReportSpecificTableBuilder
+    {
+        private static ServiceStack.Logging.ILog Log = ServiceStack.Logging.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static void AddReportSpecificTables(DataSet dataSet)
+        {
+            try
+            {
+                Common common = (Common)dataSet.Tables["RunReportRequest"].Rows[0]["CommonLibrary"];
+
+                DataTable settingsTable = dataSet.Tables["ReportSettings"];
+                settingsTable.Columns.Add("ReportTitle", typeof(string));
+                settingsTable.Rows[0]["ReportTitle"] = "USGS Daily Mean";
+
+                DataTable table = new DataTable("USGSDailyMeanDataTable");
+
+                dataSet.Tables.Add(table);
+            }
+            catch (Exception exp)
+            {
+                Log.Error("Error creating report specific data tables ", exp);
+                throw exp;
+            }
+        }
+    }
+}
---
+++ src/Reports/USGSDailyMean/USGSDailyMean.cs
@@ -0,0 +1,12 @@
+﻿using ReportPluginFramework.Beta;
+
+namespace Reports
+{
+    public class USGSDailyMean : USGSDailyMeanNamespace.ReportPluginBase, IFileReport
+    {
+        public override void AddReportSpecificTables(System.Data.DataSet dataSet)
+        {
+            USGSDailyMeanNamespace.ReportSpecificTableBuilder.AddReportSpecificTables(dataSet);
+        }
+    }
+}
