44bcde7680e46e319d76109647137e9af06b13d5
Kim Sharpe <kim.sharpe@aquaticinformatics.com>
2019-11-13 16:14:38 -0800

    Kim045-Add-New-Report-Wsc-Twelve-Month-Daily-Mean Add new custom report for Wsc called Twelve Month Daily Mean

---
+++ src/Reports/TwelveMonthDailyMean/TwelveMonthDailyMean.cs
@@ -0,0 +1,12 @@
+﻿using ReportPluginFramework.Beta;
+
+namespace Reports
+{
+    public class TwelveMonthDailyMean : TwelveMonthDailyMeanNamespace.ReportPluginBase, IFileReport
+    {
+        public override void AddReportSpecificTables(System.Data.DataSet dataSet)
+        {
+            TwelveMonthDailyMeanNamespace.ReportSpecificTableBuilder.AddReportSpecificTables(dataSet);
+        }
+    }
+}
