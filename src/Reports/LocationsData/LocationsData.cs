fa7b101ff8f1657760ccf47a1f66aef4fb5cbefd
Kim Sharpe <kim.sharpe@aquaticinformatics.com>
2020-07-13 09:21:10 -0700

    Kim072-BCMOE-Add-Custom-Report-LocationsData
    Add new custom report for BCMOE called "Locations Data"

---
+++ src/Reports/LocationsData/LocationsData.cs
@@ -0,0 +1,12 @@
+﻿using ReportPluginFramework;
+
+namespace Reports
+{
+    public class LocationsData : LocationsDataNamespace.ReportPluginBase, IFileReport
+    {
+        public override void AddReportSpecificTables(System.Data.DataSet dataSet)
+        {
+            LocationsDataNamespace.ReportSpecificTableBuilder.AddReportSpecificTables(dataSet);
+        }
+    }
+}
