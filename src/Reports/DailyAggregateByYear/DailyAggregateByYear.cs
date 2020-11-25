2827bb5cba86de3d4b151c26d112cf91decd6c6d
Kim Sharpe <kim.sharpe@aquaticinformatics.com>
2020-11-25 14:15:17 -0800

    Kim088-DEW-New-Custom-Report-DailyAggregateByYear
    add new custom report for DEW called DailyAggregateByYear

---
+++ src/Reports/DailyAggregateByYear/DailyAggregateByYear.cs
@@ -0,0 +1,12 @@
+﻿using ReportPluginFramework;
+
+namespace Reports
+{
+    public class DailyAggregateByYear : DailyAggregateByYearNamespace.ReportPluginBase, IFileReport
+    {
+        public override void AddReportSpecificTables(System.Data.DataSet dataSet)
+        {
+            DailyAggregateByYearNamespace.ReportSpecificTableBuilder.AddReportSpecificTables(dataSet);
+        }
+    }
+}
