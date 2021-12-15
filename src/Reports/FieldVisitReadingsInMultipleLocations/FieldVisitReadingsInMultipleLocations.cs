31c6055b4ce288ffa334ce6f8c0cb1fb387ac304
Kim Sharpe <kim.sharpe@aquaticinformatics.com>
2021-12-15 11:19:13 -0800

    Kim109-NWFMWD-Add-New-Report-FieldVisitReadingsInMultipleLocations
    Add new custom report for Florida NWFMWD - details in wrike 804389268

---
+++ src/Reports/FieldVisitReadingsInMultipleLocations/FieldVisitReadingsInMultipleLocations.cs
@@ -0,0 +1,12 @@
+﻿using ReportPluginFramework;
+
+namespace Reports
+{
+    public class FieldVisitReadingsInMultipleLocations : FieldVisitReadingsInMultipleLocationsNamespace.ReportPluginBase, IFileReport
+    {
+        public override void AddReportSpecificTables(System.Data.DataSet dataSet)
+        {
+            FieldVisitReadingsInMultipleLocationsNamespace.ReportSpecificTableBuilder.AddReportSpecificTables(dataSet);
+        }
+    }
+}
