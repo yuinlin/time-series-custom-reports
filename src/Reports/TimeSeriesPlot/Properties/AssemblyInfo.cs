86079cc114dd3d1d18518fac88f2d51168666bc1
DESKTOP-QFF3FGH\robertof <roberto.francesconi@aquaticinformatics.com>
2022-12-23 16:52:51 +1100

    Label "Interval Count" changed to "Intervals Per Page"

--- src/Reports/TimeSeriesPlot/Properties/AssemblyInfo.cs
+++ src/Reports/TimeSeriesPlot/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("21.1.23.0")]
-[assembly: AssemblyFileVersion("21.1.23.0")]
+[assembly: AssemblyFileVersion("21.1.24.0")]
